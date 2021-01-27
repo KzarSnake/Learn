a8a3f15f95e6a651124467d08ceb098a9fa3ac14
Denis <dragonden1@yandex.ru>
2021-01-27 20:39:58 +0300

    first commit

---
+++ js/script (var types).js
@@ -0,0 +1,27 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4/0); // infinity
+console.log('strict' * 9); // NaN
+
+const persone = 'Alex';
+
+const bool = true;
+
+// console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+
+console.log(obj.name);
+console.log(obj ["name"]);
+
+let arr = ['plum.jpg', 'orange.jpg', 'apple.bmp'];
+console.log(arr[1]);
